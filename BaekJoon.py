5a1aca0d4565d2ecfc367fd5527156e1456f2507
NeulboGom <cheerfulgenie73@gmail.com>
2023-08-21 14:51:45 +0900

    In/Out & Four Arithmetic Operations 1

---
+++ BaekJoon.py
@@ -0,0 +1,29 @@
+# 백준 1000번 문제
+A, B = input().split(" ")
+
+print(int(A) + int(B))
+
+
+# 백준 1001번 문제
+A, B = input().split(" ")
+
+print(int(A) - int(B))
+
+# 10998
+A, B = input().split(" ")
+
+print(int(A) * int(B))
+
+
+# 1008
+A, B = input().split(" ")
+
+print(int(A) / int(B))
+
+# 10869
+A, B = input().split(" ")
+print(int(A) + int(B))
+print(int(A) - int(B))
+print(int(A) * int(B))
+print(int(int(A) / int(B)))
+print(int(A) % int(B))
